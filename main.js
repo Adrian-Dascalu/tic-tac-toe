83c18c8e9c1af0ea97d7d2b703cfef495138aab2
GoldFusion1 <adiadrian9798@gmail.com>
2022-08-13 11:53:17 +0300

    optim+

--- main.js
+++ main.js
@@ -48,40 +48,38 @@ function restart() {
 }
 
 function calcul(v) {
-    if(victorie == 0)
-    {
+    if (victorie == 0) {
         if (v == 1) {
             var messageEl = document.getElementById("message-el-1");
         }
-        else if (v == 2){
+        else if (v == 2) {
             var messageEl = document.getElementById("message-el-2");
         }
-        else if (v == 3){
+        else if (v == 3) {
             var messageEl = document.getElementById("message-el-3");
         }
-        else if (v == 4){
+        else if (v == 4) {
             var messageEl = document.getElementById("message-el-4");
         }
-        else if (v == 5){
+        else if (v == 5) {
             var messageEl = document.getElementById("message-el-5");
         }
-        else if (v == 6){
+        else if (v == 6) {
             var messageEl = document.getElementById("message-el-6");
         }
-        else if (v == 7){
+        else if (v == 7) {
             var messageEl = document.getElementById("message-el-7");
         }
-        else if (v == 8){
+        else if (v == 8) {
             var messageEl = document.getElementById("message-el-8");
         }
-        else if (v == 9){
+        else if (v == 9) {
             var messageEl = document.getElementById("message-el-9");
         }
 
         if (c[v] == 0)
         {
-            if (numar == 0)
-            {
+            if (numar == 0) {
                 messageEl.textContent = "X";
 
                 numar = 1;
@@ -89,15 +87,8 @@ function calcul(v) {
                 c[v] = 1;
 
                 messaget.textContent = "O turn";
-
-                if (((c[1] == c[2]) && (c[1] == c[3]) && (c[1] != 0)) || ((c[4] == c[5]) && (c[4] == c[6]) && (c[4] != 0)) || ((c[7] == c[8]) && (c[7] == c[9]) && (c[7] != 0)) || ((c[1] == c[4]) && (c[1] == c[7]) && (c[1] != 0)) || ((c[2] == c[5]) && (c[2] == c[8]) && (c[2] != 0)) || ((c[3] == c[6]) && (c[3] == c[9]) && (c[3] != 0)) || ((c[1] == c[5]) && (c[1] == c[9]) && (c[1] != 0)) || ((c[3] == c[5]) && (c[3] == c[7]) && (c[3] != 0)))
-                {
-                    victorie = 1;
-                    message = "X e castigatorul suprem!";
-                }
             }
-            else
-            {
+            else {
                 messageEl.textContent = "O";
 
                 numar = 0;
@@ -105,18 +96,21 @@ function calcul(v) {
                 c[v] = 2;
 
                 messaget.textContent = "X turn"
+            }
 
-                if (((c[1] == c[2]) && (c[1] == c[3]) && (c[1] != 0)) || ((c[4] == c[5]) && (c[4] == c[6]) && (c[4] != 0)) || ((c[7] == c[8]) && (c[7] == c[9]) && (c[7] != 0)) || ((c[1] == c[4]) && (c[1] == c[7]) && (c[1] != 0)) || ((c[2] == c[5]) && (c[2] == c[8]) && (c[2] != 0)) || ((c[3] == c[6]) && (c[3] == c[9]) && (c[3] != 0)) || ((c[1] == c[5]) && (c[1] == c[9]) && (c[1] != 0)) || ((c[3] == c[5]) && (c[3] == c[7]) && (c[3] != 0)))
-                {
-                    victorie = 1;
+            if (((c[1] == c[2]) && (c[1] == c[3]) && (c[1] != 0)) || ((c[4] == c[5]) && (c[4] == c[6]) && (c[4] != 0)) || ((c[7] == c[8]) && (c[7] == c[9]) && (c[7] != 0)) || ((c[1] == c[4]) && (c[1] == c[7]) && (c[1] != 0)) || ((c[2] == c[5]) && (c[2] == c[8]) && (c[2] != 0)) || ((c[3] == c[6]) && (c[3] == c[9]) && (c[3] != 0)) || ((c[1] == c[5]) && (c[1] == c[9]) && (c[1] != 0)) || ((c[3] == c[5]) && (c[3] == c[7]) && (c[3] != 0))) {
+                victorie = 1;
+                if (numar == 0) {
                     message = "O e castigatorul suprem!";
+                }
+                else {
+                    message = "X e castigatorul suprem!";
                 }
             }
         }
     }
 
-    if (victorie == 1)
-    {
+    if (victorie == 1) {
         messageEl = document.getElementById("message-e");
 
         messageEl.textContent = message;
